60bf8a4b1fc44b4d2a9abb713133e314f37b7fb0
YarL03 <yaroslav.labetsky@yandex.ru>
2022-09-29 23:10:55 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,10 +79,54 @@ module.exports = {
   plugins: [
     plugin(({addComponents, theme, addUtilities}) => {
       addComponents({
-        'btn-primary': {
+        '.btn-primary': {
           backgroundColor: primary,
           color: '#fff',
-          borderRadius: '0.65rem'
+          borderRadius: '0.65rem',
+          transition: 'background-color .3s ease-in-out',
+          '&:hover': {
+            backgroundColor: '#ff0009'
+          }
+        },
+
+        '.text-link': {
+          textUnderlineOffset: 4,
+          color: 'rgba(255, 255, 255, 0.2)',
+          transition: 'text-decoration-color .3s ease-in-out',
+          textDecorationLine: 'underline',
+          textDecorationColor: 'rgba(255, 255, 255, 0.2)',
+          '&:hover': {
+            textDecorationColor: 'rgba(255, 255, 255, 0.9)'
+          }
+        },
+
+        '.air-block': {
+          borderRadius: theme('borderRadius.layout'),
+          backgroundColor: theme('colors.grey.950'),
+          color: theme('colors.white'),
+          boxShadow: theme('boxShadow.lg')
+        }
+      }),
+      addUtilities({
+        '.text-shadow': {
+          textShadow: '1px 1px rgba(0, 0, 0, 0.4)'
+        },
+
+        '.outline-border-none': {
+          outline: 'none',
+          border: 'none',
+        },
+
+        '.flex-center-between': {
+          diplay: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        },
+
+        '.image-like-bg': {
+          objectPosition: 'center',
+          objectFit: 'cover',
+          pointerEvents: 'none'
         }
       })
     })
