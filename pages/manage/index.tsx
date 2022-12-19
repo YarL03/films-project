14c97133f0d781a0ac1f42cc9e94fd302910b88b
YarL03 <yaroslav.labetsky@yandex.ru>
2022-12-19 22:52:59 +0800

    feat: axios interceptor

---
+++ pages/manage/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+const AdminPage:NextPage = () => {
+    return (
+        <></>
+    )
+}
+
+export default AdminPage
---
+++ pages/profile.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next"
+
+const ProfilePage:NextPage = () => {
+    return (
+        <></>
+    )
+}
+
+export default ProfilePage
